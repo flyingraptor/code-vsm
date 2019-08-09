7498e7ae8bcca16cc019014fea275946b2472064
flyingraptor <nikolaos.raptis83@gmail.com>
2019-08-09 17:01:12 +0300

    Prepare the test case to have a test git repo and delete it after finish

---
+++ cvsm-git-parser/src/test/java/org/code/vsm/git/parser/SingleBranchShowStatisticsTest.java
@@ -0,0 +1,42 @@
+package org.code.vsm.git.parser;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.apache.commons.io.FileUtils;
+
+@Tag("AcceptanceTest")
+public class SingleBranchShowStatisticsTest {
+	
+	private static File gitTestDirectory; 
+	
+	@BeforeAll
+	public static void createGitRepositoryForTest() throws IOException, IllegalStateException, GitAPIException {
+		
+        gitTestDirectory = File.createTempFile("cvsm", ".test");
+        
+        if(!gitTestDirectory.delete()) {
+            throw new IOException("Could not delete previous file " + gitTestDirectory);
+        }
+        
+        Git.init().setDirectory(gitTestDirectory).call();
+	}
+
+	@Test
+	public void testStatisticsCorrectForAGivenBranch() {
+		assertTrue(false);
+	}
+	
+	@AfterAll
+	public static void deleteGitRepositoryForTest() throws IOException, IllegalStateException, GitAPIException {
+		FileUtils.deleteDirectory(gitTestDirectory);
+	}
+}
