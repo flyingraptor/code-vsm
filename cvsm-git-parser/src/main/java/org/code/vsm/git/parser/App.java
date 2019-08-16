38941f1d9c5b160124c00aa828e98bed428e117f
flyingraptor <nikolaos.raptis83@gmail.com>
2019-08-16 20:25:34 +0300

    Playing with JGit and Commits...

--- cvsm-git-parser/src/main/java/org/code/vsm/git/parser/App.java
+++ cvsm-git-parser/src/main/java/org/code/vsm/git/parser/App.java
@@ -1,6 +1,8 @@
 package org.code.vsm.git.parser;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
@@ -14,6 +16,9 @@ import org.eclipse.jgit.revwalk.RevCommit;
  */
 public class App 
 {
+	
+	private static File gitTestDirectory; 
+	
     public static void main( String[] args ) throws IllegalStateException, IOException, GitAPIException
     {
         System.out.println( "Hello World!" );
@@ -22,20 +27,58 @@ public class App
     
     private static void nucfield() throws IOException, IllegalStateException, GitAPIException {
     	
-//    	File localPath = File.createTempFile("TestGitRepository", "");
-//    	
-//    	Git git = Git.init().setDirectory(localPath).call();
-//        System.out.println("Having repository: " + git.getRepository().getDirectory());
-//        
-//        Iterable<RevCommit> commits = git.log().all().call();
-//        int count = 0;
-//        for (RevCommit commit : commits) {
-//            System.out.println("LogCommit: " + commit);
-//            count++;
-//        }
-//        System.out.println(count);
-//        
-//        FileUtils.deleteDirectory(localPath);
+    	gitTestDirectory = File.createTempFile("cvsm", ".test");  
+    	if(!gitTestDirectory.delete()) {
+            throw new IOException("Could not delete temporary file " + gitTestDirectory);
+        }
+    	
+    	try (Git git = Git.init().setDirectory(gitTestDirectory).call()) {
+    		
+            System.out.println("Having repository: " + git.getRepository().getDirectory());
+            
+            File myFile = new File(git.getRepository().getDirectory().getParent(), "testfile");
+            if(!myFile.createNewFile()) {
+                throw new IOException("Could not create file " + myFile);
+            }
+            
+            String str = "This is it. Try to find way to make the prog work.\n";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(myFile));
+            writer.write(str);
+             
+            writer.close();
+            
+            git.add()
+	            .addFilepattern("testfile")
+	            .call();
+            
+            git.commit()
+	            .setMessage("Added testfile")
+	            .call();
+            
+            String str2 = "This is it. Try to find way to make the prog work. And I added more text\n";
+            BufferedWriter writer2 = new BufferedWriter(new FileWriter(myFile));
+            writer2.write(str2);
+             
+            git.commit()
+            .setMessage("Added testfile")
+            .call();
+            
+            writer2.close();
+
+            System.out.println("Added file " + myFile + " to repository at " 
+            + git.getRepository().getDirectory());
+            
+        	Iterable<RevCommit> commits = git.log().all().call();
+            int count = 0;
+            for (RevCommit commit : commits) {
+                System.out.println("LogCommit: " + commit);
+                count++;
+            }
+            System.out.println(count);
+
+            
+            //FileUtils.deleteDirectory(gitTestDirectory);
+        }
         
     }
 }
